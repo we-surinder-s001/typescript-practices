260fe05605f4037e96f88fab2eb09505ceb93d5f
Surinder Singh <surinder-s001@webreinvent.com>
2024-01-24 13:06:03 +0530

    first commit.

---
+++ index.ts
@@ -0,0 +1,9 @@
+let Name: string = "Surinder Singh";
+
+console.log(Name);
+
+const sum = (a: number, b: number): number => {
+    return (a + b);
+}
+
+console.log(sum(1, 2));
---
+++ output/index.js
@@ -0,0 +1,6 @@
+var Name = "Surinder Singh";
+console.log(Name);
+var sum = function (a, b) {
+    return (a + b);
+};
+console.log(sum(1, 2));
